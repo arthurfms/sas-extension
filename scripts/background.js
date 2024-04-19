964ee8625a41aadb823315e2579dfa3de25e715a
Arthur Santos <arthurfernandes1993@gmail.com>
2024-04-19 08:26:12 -0300

    Change to manifest_version 3

--- scripts/background.js
+++ scripts/background.js
@@ -1,4 +1,4 @@
-
+console.log("BG HERE");
 const getOptions = () => {
   chrome.storage.sync.get(
     {
@@ -18,13 +18,22 @@ const getOptions = () => {
       testAffiliate: true,
     },
     (items) => {
-      if (items.menucontext && (items.merContext || items.itpContext || items.testLinkContext || items.affContext)) {
+      if (
+        items.menucontext &&
+        (items.merContext ||
+          items.itpContext ||
+          items.testLinkContext ||
+          items.affContext)
+      ) {
+        let contextOptions = {};
 
-        let contextOptions = {}
-
-        items.merContext ? contextOptions.merchant = 'Go to Merchant: %s' : '';
-        items.itpContext ? contextOptions.itp = 'Merchant ITP: %s' : '';
-        items.affContext ? contextOptions.affiliate = 'Go to Affiliate: %s' : '';
+        items.merContext
+          ? (contextOptions.merchant = "Go to Merchant: %s")
+          : "";
+        items.itpContext ? (contextOptions.itp = "Merchant ITP: %s") : "";
+        items.affContext
+          ? (contextOptions.affiliate = "Go to Affiliate: %s")
+          : "";
 
         // Add a listener to create the initial context menu items,
         // context menu items only need to be created at runtime.onInstalled
@@ -32,36 +41,43 @@ const getOptions = () => {
           chrome.contextMenus.create({
             id: op,
             title: title,
-            type: 'normal',
-            contexts: ['selection']
+            type: "normal",
+            contexts: ["selection"],
           });
         }
-        
+
         // Open a new search tab when the user clicks a context menu
         chrome.contextMenus.onClicked.addListener((item, tab) => {
           let opId = item.menuItemId;
           let url;
           let validatedUrl;
-        
+
           switch (opId) {
             case "merchant":
-              validatedUrl = (/^\d+$/.test(item.selectionText.trim()) && parseInt(item.selectionText.trim()) > 95) ? `https://account.shareasale.com/admin/adminDetailsMerchant.cfm?merchantId=${item.selectionText.trim()}&searchby=${item.selectionText.trim()}` : `https://account.shareasale.com/admin/index.cfm?searchby=${item.selectionText.trim()}&blnUserSearch=1&searchFor=merchants`;
+              validatedUrl =
+                /^\d+$/.test(item.selectionText.trim()) &&
+                parseInt(item.selectionText.trim()) > 95
+                  ? `https://account.shareasale.com/admin/adminDetailsMerchant.cfm?merchantId=${item.selectionText.trim()}&searchby=${item.selectionText.trim()}`
+                  : `https://account.shareasale.com/admin/index.cfm?searchby=${item.selectionText.trim()}&blnUserSearch=1&searchFor=merchants`;
               url = new URL(validatedUrl);
-              break
-              case "itp":
-                validatedUrl = `https://account.shareasale.com/admin/itp.cfm?merchantid=${item.selectionText.trim()}&searchby=${item.selectionText.trim()}`;
-                url = new URL(validatedUrl);
-              break
-              case "affiliate":
-                validatedUrl = (/^\d+$/.test(item.selectionText.trim()) && parseInt(item.selectionText.trim()) > 24) ? `https://account.shareasale.com/admin/adminDetailsAffiliate.cfm?userid=${item.selectionText.trim()}&searchby=${item.selectionText.trim()}` : `https://account.shareasale.com/admin/index.cfm?searchby=${item.selectionText.trim()}&blnUserSearch=1&searchFor=users`;
+              break;
+            case "itp":
+              validatedUrl = `https://account.shareasale.com/admin/itp.cfm?merchantid=${item.selectionText.trim()}&searchby=${item.selectionText.trim()}`;
               url = new URL(validatedUrl);
-              break
+              break;
+            case "affiliate":
+              validatedUrl =
+                /^\d+$/.test(item.selectionText.trim()) &&
+                parseInt(item.selectionText.trim()) > 24
+                  ? `https://account.shareasale.com/admin/adminDetailsAffiliate.cfm?userid=${item.selectionText.trim()}&searchby=${item.selectionText.trim()}`
+                  : `https://account.shareasale.com/admin/index.cfm?searchby=${item.selectionText.trim()}&blnUserSearch=1&searchFor=users`;
+              url = new URL(validatedUrl);
+              break;
           }
-        
+
           chrome.tabs.create({ url: url.href, index: tab.index + 1 });
         });
       }
-      
     } // end items
   );
 };
